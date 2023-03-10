5dc0f5cc767732de2a25dfe4eb09831507fdf319
DariushG3 <ghasemi.dariush@yahoo.com>
2023-03-10 18:32:53 +0100

    Checking cases excluded for the thyroid disorder/therapy

--- 09_interaction_analysis.R
+++ 09_interaction_analysis.R
@@ -19,6 +19,7 @@ vcfReg_TSHmod <-
   vcfReg %>%
   mutate(Thyroid_DrugName = na_if(Thyroid_DrugName, "")) %>%
   filter(
+    # Finally, 416 cases were removed after imposing below filtering
     !if_all(c(Thyroid_DrugName, TSH), is.na),                        # Removed are:   4 with NAs
     #Cancer                 != 1 | is.na(Cancer),                    # Removed are: 356 with Cancer
     Thyroid_cancer          != 1 | is.na(Thyroid_cancer),            # Removed are:  16 with Thyroid_cancer (only 3 more removed after removing Cancer cases)
@@ -27,10 +28,10 @@ vcfReg_TSHmod <-
     Operation_thyroid_gland != 1 | is.na(Operation_thyroid_gland),   # Removed are: 312 with operation on thyroid gland
     #Alteration_thyroid_pregnancy != 1 | is.na(Alteration_thyroid_pregnancy), # Removed are: 64 removed (but 62 should be eliminated)
     ) %>%
-  mutate(TSH_cat = replace(TSH_cat, Thyroid_DrugName == "Iodine therapy",       "HyperT"),
-         TSH_cat = replace(TSH_cat, Thyroid_DrugName == "Levothyroxine sodium", "HypoT"),
-         TSH_cat = replace(TSH_cat, Thyroid_DrugName == "Propylthiouracil",     "HyperT"),
-         TSH_cat = replace(TSH_cat, Thyroid_DrugName == "Thiamazole",           "HyperT"),
+  mutate(TSH_cat = replace(TSH_cat, Thyroid_DrugName == "Iodine therapy",       "HyperT"), # no cases taking iodine therapy
+         TSH_cat = replace(TSH_cat, Thyroid_DrugName == "Levothyroxine sodium", "HypoT"),  # 512 cases taking Levothyn
+         TSH_cat = replace(TSH_cat, Thyroid_DrugName == "Propylthiouracil",     "HyperT"), # 1 case  taking Propylthiouracil
+         TSH_cat = replace(TSH_cat, Thyroid_DrugName == "Thiamazole",           "HyperT"), # 3 cases taking thiamazole
          #Changing the reference level of TSH_cat to TSH_cat = "2" or "NormT"
          TSH_cat = relevel(as.factor(TSH_cat), ref = 2)) 
 #count(TSH_cat)
