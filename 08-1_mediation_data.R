6743351635895cfbfca6d585575942a960747c65
dariushghasemi <ghasemi.dariush@yahoo.com>
2025-02-17 23:59:41 +0100

    update script merging phenotype and genotype

--- 08-1_mediation_data.R
+++ 08-1_mediation_data.R
@@ -6,61 +6,51 @@ library(tidyverse)
 library(stringr)
 library(purrr)
 
+path_dosage <- "inputs/163Targets.txt"
+path_pcs <- "../HaploReg/data/CHRIS13K.GT.evecs"
+path_clinicals <- "../HaploReg/data/chris_q-norm.csv"
 
 #------------------------------------------------------#
-#------------ Merging Genotypes & Phenotypes ----------
+#------               Reading data              -------
 #------------------------------------------------------#
 
 
 # Genotypes file containing the Dosage levels of the alleles of the SNPs
-vcf <- read.delim("D:\\Dariush\\PhD\\Analysis\\Data\\163Targets.txt",
-                  col.names = c("AID", "MARKER_ID", "Dosage"), sep = "\t", stringsAsFactors = FALSE)
-#------------#
+vcf <- data.table::fread(path_dosage, col.names = c("AID", "MARKER_ID", "Dosage"))
 
 # Principle components
-PCs_raw <- read.delim("D:\\Dariush\\PhD\\Analysis\\Data\\CHRIS.10K.genotypes.evecs", 
-                      sep = "\t", stringsAsFactors = FALSE)
+pcs <- data.table::fread(path_pcs) %>% rename(AID = "#IND_ID")
 
-PCs_13K <- read.delim("D:\\Dariush\\PhD\\Analysis\\Data\\CHRIS13K.GT.evecs", sep = "\t", 
-                      stringsAsFactors = FALSE)
+# load clinical traits
+chris <- read.csv(path_clinicals)
 
-PCs_13K <- PCs_13K %>% rename(AID = X.IND_ID)
 
 #------------#
 # Genotypes file in wide format
 vcfmod <- vcf %>%
-  mutate_at("MARKER_ID", 
-            str_replace_all, 
-            ":[A-T-C-G]+:[A-T-C-G]+", "")%>%
-  rename(SNPid = MARKER_ID) %>%
-  pivot_wider(names_from = "SNPid",
-              values_from= "Dosage")%>% 
-  select(-c("chr11:78335892", "chr11:78392251"))
-#------------#
+  dplyr::mutate_at("MARKER_ID", str_replace_all, ":[A-T-C-G]+:[A-T-C-G]+", "") %>%
+  dplyr::rename(SNPid = MARKER_ID) %>%
+  pivot_wider(names_from = "SNPid", values_from= "Dosage") %>% 
+  dplyr::select(-c("chr11:78335892", "chr11:78392251"))
+
+
+#------------------------------------------------------#
+#------      Merging Genotypes & Phenotypes      ------
+#------------------------------------------------------#
 
 # Combining dosages + kidney-thyroid traits + PCs
 # This dataset is being used for mediation analysis.
 # Later we will used this data to filter cases to be 
 # used in interaction analyses with TSH and thyroid disease.
 
-vcfReg <- chris[c(
-  "AID",
-  "Sex",
-  "Age",
-  "Operator",
-  "Municipality",
-  "Cancer",
-  "Thyroid_cancer",
-  "Alteration_thyroid_pregnancy",
-  "Thyroid_DrugName",
-  "kidneyCancer",
-  "Goiter",
-  "Operation_thyroid_gland",
-  "TSH_cat",
-  "eGFRw.log.Res",
-  "eGFRw.log",
-  "eGFR",
-  quantVars)] %>% 
+vcfReg <- chris %>%
+  dplyr::select(
+    AID, Sex, Age, Operator, Municipality,
+    Cancer, Thyroid_cancer, Alteration_thyroid_pregnancy,
+    Thyroid_DrugName, kidneyCancer, Goiter, Operation_thyroid_gland,
+    TSH_cat, eGFRw.log.Res, eGFRw.log, eGFR,
+    all_of(quantVars)
+  ) %>%
   inner_join(vcfmod,  by = "AID") %>%
-  inner_join(PCs_13K, by = "AID", all = FALSE)
-#------------#
+  inner_join(pcs, by = "AID", all = FALSE)
+
